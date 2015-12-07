dd9f63b3b3d6bca017bb8b350372a1d45a185aad
adamjakab <jakabadambalazs@gmail.com>
2015-12-07 02:25:37 +0100

    added functional tests - needs refactoring

--- test/functional/tests/ServiceSpec.js
+++ test/functional/tests/ServiceSpec.js
@@ -3,15 +3,57 @@
  */
 var expect = require("chai").expect;
 var http = require("http");
+var syncRequest = require("sync-request");
+var querystring = require("querystring");
+var path = require('path');
+var exec = require('child_process').exec;
+var sleep = require('sleep');
+var config = require("../../../configuration.json");
+
+var projectPath = path.resolve(__dirname, "../../../");
+var indexJs = path.resolve(projectPath, "index.js");
+var command = 'node ' + indexJs;
+var cmdOpts = {
+    cwd: projectPath
+};
+
+var address = config.server.ip;
+address = (address ? address : "127.0.0.1");
+var port = config.server.port;
+
+/**
+ * Using sync-request(https://github.com/ForbesLindesay/sync-request) for testing
+ */
+
+
 
 describe("Service Tests", function () {
-
     describe("#start service", function () {
-        it("should start 1", function () {
-
-
-            expect(true).to.be.equal(true);
+        this.timeout(5000);
+        it("should be available withing 5 seconds", function () {
+            var child = exec(command, cmdOpts);
+            //console.log("Command started with pid: " + child.pid);
+            var attempts = 0;
+            while (true) {
+                attempts++;
+                var res = false;
+                try {
+                    res = syncRequest("POST", 'http://' + address + ":" + port);
+                } catch (e) {/**/
+                }
+                if (res || attempts > 20) {
+                    break;
+                }
+                sleep.usleep(250);
+            }
+            //console.log("RES: " + JSON.stringify(res));
+            expect(res).not.to.be.equal(false);
+            expect(res).to.have.property('statusCode', 500);
+            child.kill('SIGTERM');
         });
     });
 
+
 });
+
+
